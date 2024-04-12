bce52b6dff14b87c24ca08fc19d32153cc274b77
Caleb Norman <lowbeenorman@gmail.com>
2024-04-12 10:08:46 -0600

    Forgot to take out unused startscreen in main

--- MinesweeperEdu/main.cpp
+++ MinesweeperEdu/main.cpp
@@ -1,12 +1,10 @@
 #include "mainwindow.h"
-#include "startscreen.h"
 
 #include <QApplication>
 
 int main(int argc, char *argv[])
 {
     QApplication a(argc, argv);
-    StartScreen ss;
     MainWindow w;
     w.show();
 
