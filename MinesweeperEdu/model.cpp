6289cfed80631072a338fd78380849c96d7429b8
Kylverr <kylverofficial@gmail.com>
2024-04-17 13:13:23 -0600

    JSON reading started

--- MinesweeperEdu/model.cpp
+++ MinesweeperEdu/model.cpp
@@ -1,3 +1,25 @@
 #include "model.h"
+#include <QJsonDocument>
+#include <QFile>
+#include <QByteArray>
 
-Model::Model() {}
+Model::Model() {
+    createLessonLevels();
+}
+
+void Model::createLessonLevels() {
+    for(int i = 0; i < 1; ++i) {
+
+    }
+}
+
+LessonLevel Model::constructLessonLevelFromJSON(QString filename) {
+    QJsonDocument doc;
+    QFile file (filename);
+    // Return nothing if file cannot be opened
+    if (!file.open (QIODevice::ReadOnly)) {}
+    //   TODO: return something
+    // array of bytes contained in file
+    QByteArray array = file.readAll ();
+    doc = QJsonDocument::fromJson (array);
+}
--- MinesweeperEdu/model.h
+++ MinesweeperEdu/model.h
@@ -2,6 +2,7 @@
 #define MODEL_H
 
 #include <QList>
+#include "lessonlevel.h"
 
 class Model
 {
@@ -11,6 +12,21 @@ public:
 private:
     //list of tips
     QList<QString> gameTips;
+    //list of all Lessons
+    QList<LessonLevel> lessons;
+
+    ///
+    /// \brief Constructs all the LessonLevels for the application.
+    ///
+    void createLessonLevels();
+
+    ///
+    /// \brief Reads a JSON file from the given filename and returns the LessonLevel
+    /// constructed from the JSON.
+    /// \param filename name of the file to construct the LessonLevel from
+    /// \return LessonLevel
+    ///
+    LessonLevel constructLessonLevelFromJSON(QString filename);
 };
 
 #endif // MODEL_H
