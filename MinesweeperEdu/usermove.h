fc4553810e7773b7a4ba7e8ccfc461d0113e8fb0
Kylverr <kylverofficial@gmail.com>
2024-04-19 22:06:24 -0600

    First JSON file added. More JSON reading done.

--- MinesweeperEdu/usermove.h
+++ MinesweeperEdu/usermove.h
@@ -1,19 +1,25 @@
 #ifndef USERMOVE_H
 #define USERMOVE_H
 #include <QPoint>
+#include <QJsonObject>
 
 class UserMove
 {
 public:
     UserMove(QPoint cell); //ToDo add parameter for Right/Left Click
     UserMove();
+    ///
+    /// \brief Builds a UserMove from JSON
+    /// \param obj
+    ///
+    UserMove(QJsonObject &obj);
     enum class MoveType
     {
         FLAG, CLEAR
     };
-    MoveType type;
 private:
     QPoint cell;
+    MoveType type;
 };
 
 #endif // USERMOVE_H
