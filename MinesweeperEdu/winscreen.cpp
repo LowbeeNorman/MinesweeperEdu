268ce4f86385f188090488d5924fe79722b1d450
winstonji <winston.wy.ji@gmail.com>
2024-04-24 18:57:52 -0600

    adjusted youWin image and fixed bounce of youWin logo

--- MinesweeperEdu/winscreen.cpp
+++ MinesweeperEdu/winscreen.cpp
@@ -49,37 +49,11 @@ void WinScreen::paintEvent(QPaintEvent *event)
 
 void WinScreen::setUpBox2D()
 {
-    // Define the ground body.
-    b2BodyDef groundBodyDef;
-
-    // Choose where the ground is
-    groundBodyDef.position.Set(0.0f, -20.0f);
-
-    // Call the body factory which allocates memory for the ground body
-    // from a pool and creates the ground box shape (also from a pool).
-    // The body is also added to the world.
-    b2Body* groundBody = world.CreateBody(&groundBodyDef);
-
-    // Define the ground box shape.
-    b2PolygonShape groundBox;
-
-    // Width of the ground, in half widths (similar to radius vs diameter)
-    groundBox.SetAsBox(50.0f, 10.0f);
-
-    // Add the ground fixture to the ground body.
-    groundBody->CreateFixture(&groundBox, 0.0f);
-
     // Define the dynamic body. We set its position and call the body factory (next line)
     b2BodyDef bodyDef;
 
     // Dynamic bodies move, static bodies do not
     bodyDef.type = b2_dynamicBody;
-
-    // Starting position of moving item
-    bodyDef.position.Set(0.0f, 0.0f);
-
-    // Angle of moving item
-    bodyDef.angle = 0.45f;
 
     // Links body to specifications we just made, through a method in world
     body = world.CreateBody(&bodyDef);
@@ -90,30 +64,6 @@ void WinScreen::setUpBox2D()
     b2Vec2 initialVelocity(std::cos(45.0f * b2_pi / 180.0f), std::sin(45.0f * b2_pi / 180.0f));
     body->SetLinearVelocity(initialVelocity);
 
-    // Slowdown rate, want it at 0 because it should not slow down
-    body->SetLinearDamping(0.0f);
-
-    // Define another box shape for our dynamic body.
-    b2PolygonShape dynamicBox;
-    dynamicBox.SetAsBox(1.0f, 1.0f);
-
-    // Define the dynamic body fixture.
-    b2FixtureDef fixtureDef;
-    fixtureDef.shape = &dynamicBox;
-
-    // Set the box density to be non-zero, so it will be dynamic.
-    fixtureDef.density = 0.0f;
-
-    // Override the default friction.
-    fixtureDef.friction = 0.0f;
-
-    // Bounciness
-    fixtureDef.restitution = 0.0f;
-
-    // Add the shape to the body.
-    body->CreateFixture(&fixtureDef);
-
-    // timer->start(1.0f / 60.0f);
     timer->setInterval (1.0f / 60.0f * 1000);
     timer->start ();
 
@@ -128,21 +78,34 @@ void WinScreen::updateWorld()
 
     // New position of the label
     b2Vec2 velocity = body->GetLinearVelocity();
-    int newX = ui->winLabel->x() + velocity.x * 5.0f;
-    int newY = ui->winLabel->y() + velocity.y * 5.0f;
+    int newX = ui->winLabel->x() + velocity.x * 2.0f;
+    int newY = ui->winLabel->y() + velocity.y * 2.0f;
 
     int labelWidth = ui->winLabel->width();
     int labelHeight = ui->winLabel->height();
 
     // Check if the new position hits any of the window edges
-    if (newX <= 0 || newX >= width() - labelWidth) {
-        // If the label hits the left or right hand side of the window, the horizontal velocity will be inverted
+    // left edge
+    if (newX < 0.19 * width()) {
         body->SetLinearVelocity(b2Vec2(-velocity.x, velocity.y));
+        newX = 0.19 * width();
     }
-    if (newY <= 0 || newY >= height() - labelHeight) {
-        // If the label hits the top or bottom side of the window, the vertical velocity will be inverted
+    //right edge
+    if (newX > 0.63 * width()) {
+        body->SetLinearVelocity(b2Vec2(-velocity.x, velocity.y));
+        newX = 0.63 * width();
+    }
+    //top edge
+    if (newY < 0.35 * height()) {
         body->SetLinearVelocity(b2Vec2(velocity.x, -velocity.y));
+        newY = 0.35 * height();
     }
+    //bottom edge
+    if (newY > 0.67 * height()) {
+        body->SetLinearVelocity(b2Vec2(velocity.x, -velocity.y));
+        newY = 0.67 * height();
+    }
+
 
     // Position is changed
     ui->winLabel->setGeometry(newX, newY, labelWidth, labelHeight);
